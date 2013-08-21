cbb3a20d9d1b86129f874f11f436b4d8639711b9
guohaozhao <babyduncan@qq.com>
2013-08-21 10:27:42 +0800

    add employee model

---
+++ myfirstoc/myfirstoc/Employee.h
@@ -0,0 +1,40 @@
+//
+//  Employee.h
+//  myfirstoc
+//
+//  Created by BabyDuncan on 13-8-21.
+//  Copyright (c) 2013年 BabyDuncan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Employee : NSObject {
+    NSString *firstName;
+    NSString *lastName;
+    NSDate *birthDate;
+    NSDate *dateOfEmployment;
+    Employee *manager;
+    NSString *ssn;
+    double salary;
+}
+
+@property(nonatomic, retain) NSString *firstName;
+@property(nonatomic, retain) NSString *lastName;
+@property(nonatomic, retain) NSDate *birthDate;
+@property(nonatomic, retain) NSDate *dateOfEmployment;
+@property(nonatomic, assign) Employee *manager;
+@property(nonatomic, retain) NSString *ssn;
+@property(nonatomic, readonly) NSTimeInterval age;
+@property(nonatomic, assign) double salary;
+
+- (id)initWithFirstName:(NSString *)inFirstName
+               lastName:(NSString *)inLastName
+              birthDate:(NSDate *)inBirthDate
+                    ssn:(NSString *)inSSN;
+
+- (void)giveRaise:(double)presentAge;
+
+- (double)bonus;
+
+
+@end
---
+++ myfirstoc/myfirstoc/Employee.m
@@ -0,0 +1,13 @@
+//
+//  Employee.m
+//  myfirstoc
+//
+//  Created by BabyDuncan on 13-8-21.
+//  Copyright (c) 2013年 BabyDuncan. All rights reserved.
+//
+
+#import "Employee.h"
+
+@implementation Employee
+
+@end
