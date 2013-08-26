f1bf69a65799b5bfa244eb594f98cca092eec83a
guohaozhao <babyduncan@qq.com>
2013-08-26 10:32:39 +0800

    add scanfUsage

---
+++ ocProgramming/cp5/scanfUsage.m
@@ -0,0 +1,10 @@
+// first oc program
+#import <Foundation/Foundation.h>
+
+int main(int argc,const char * argv [] ){
+    @autoreleasepool{
+	int i;
+	scanf("%d",&i);
+	NSLog(@"your input is %d",i);
+    }
+}
