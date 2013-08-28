394646ecb438ab8b6b3bb3b809fc2b9c72093196
guohaozhao <babyduncan@qq.com>
2013-08-28 14:59:01 +0800

    add AFnetworking

--- ochc/ochcTests/ochcTests.m
+++ ochc/ochcTests/ochcTests.m
@@ -10,23 +10,33 @@
 
 @implementation ochcTests
 
-- (void)setUp
-{
+- (void)setUp {
     [super setUp];
-    
+
     // Set-up code here.
 }
 
-- (void)tearDown
-{
+- (void)tearDown {
     // Tear-down code here.
-    
+
     [super tearDown];
 }
 
-- (void)testExample
-{
+- (void)testExample {
     NSLog(@"test!!");
+    __block id blockResponseObject = nil;
+
+    NSURLRequest *request = [NSURLRequest requestWithURL:@"http://10.2.58.121:8080/v2/profile/index.htm"];
+    AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc] initWithRequest:request];
+    [operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+        blockResponseObject = responseObject;
+        NSLog(@"OK");
+    }                                failure:nil];
+
+    [operation start];
+//    expect([operation isFinished]).will.beTruthy();
+//    expect(blockResponseObject).willNot.beNil();
+
 }
 
 @end
